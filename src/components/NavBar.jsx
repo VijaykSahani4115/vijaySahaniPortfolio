02dfe4bbd700fbb8e49fb0b814d2b15d3ba5cad2
Vijaytijs <vijays@tijsindia.com>
2025-06-13 15:54:38 +0530

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,9 +30,9 @@ export default function NavBar() {
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
-              src="/dev-icon.svg"
+              src="/dev-icon.png"
               className="mr-3 h-6 sm:h-9"
-              alt="Vijay Dev Logo"
+              alt=""
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
              VijaySahani.dev
